d97ce9e77b2df3a34a969664e4584d784512355f
syarifalhuda <syarifalhuda260@gmail.com>
2020-09-12 17:05:24 +0700

    pembuatan file index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+echo "Animal <php>";
+echo " <hr>";
+
+class kucing{
+	   public $nama,  $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$kucing =  new kucing;
+	$kucing->nama = "cemong";
+	$kucing->jumlah_kaki = "4";
+	$kucing->bisa_terbang = "tidak";
+	$kucing->suara = "meong";
+
+	echo "kucing  <br>";
+	echo "nama : $kucing->nama <br>";
+	echo "jumlah kaki : $kucing->jumlah_kaki <br>";
+	echo "bisa terbang : $kucing->bisa_terbang <br>";
+	echo "suara : $kucing->suara <br>";
+	echo "<hr>";
+
+class anjing{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$anjing = new anjing;
+	$anjing->nama = "rizki";
+	$anjing->jumlah_kaki = "4";
+	$anjing->bisa_terbang = "tidak";
+	$anjing->suara = "gukgukguk";
+
+
+	echo "anjing  <br>";
+	echo "nama : $anjing->nama <br>";
+	echo "jumlah kaki : $anjing->jumlah_kaki <br>";
+	echo "bisa terbang : $anjing->bisa_terbang <br>";
+	echo "suara : $anjing->suara <br>";
+	echo "<hr>";
+
+	class elang{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$elang = new elang;
+	$elang->nama = "jony";
+	$elang->jumlah_kaki = "2";
+	$elang->bisa_terbang = "ya";
+	$elang->suara = "aaaaaaa";
+
+	echo "elang  <br>";
+	echo "nama : $elang->nama <br>";
+	echo "jumlah kaki : $elang->jumlah_kaki <br>";
+	echo "bisa terbang : $elang->bisa_terbang <br>";
+	echo "suara : $elang->suara <br>";
+	echo "<hr>";
+
+	
+	class angsa{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$angsa = new angsa;
+	$angsa->nama = "siska";
+	$angsa->jumlah_kaki = "2";
+	$angsa->bisa_terbang = "ya";
+	$angsa->suara = "kukuriuuk";
+
+	echo "angsa <br>";
+	echo "nama : $angsa->nama <br>";
+	echo "jumlah kaki : $angsa->jumlah_kaki <br>";
+	echo "bisa terbang : $angsa->bisa_terbang <br>";
+	echo "suara : $angsa->suara <br>";
+	echo "<hr>";
